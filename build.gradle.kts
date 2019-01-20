b2e0623053468beb4e1a0ec28f3efbe12cb000f5
fachher.syed <fachher@googlemail.com>
2019-01-20 18:26:04 +0100

    replaced dependency org.objenesis:objenesis:2.6 by 3.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,24 @@
 plugins {
     java
+    id("base") // only writting "base" with id(*) would work too
 }
 
 group = "de.syed"
 version = "1.0-SNAPSHOT"
+description = "A simple gradle school project"
 
 allprojects {
     print("allprojects sections\n")
+
+    configurations.all {
+        resolutionStrategy{
+            dependencySubstitution {
+                substitute(module("org.objenesis:objenesis:2.6"))
+                        .because("I just want to take advantage of the new features")
+                        .with(module("org.objenesis:objenesis:3.0.1"))
+            }
+        }
+    }
 }
 
 artifacts {
@@ -33,4 +45,21 @@ dependencies {
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+// tasks
+tasks.create<Copy>("copy") {
+    description = "Copies sources to the dest directory"
+    group = "Custom"
+    DuplicatesStrategy.WARN
+    from("src")
+    into("dest")
+}
+// belongs to base plugin
+tasks.create<Zip>("zip") {
+    description = "Archives sources in a zip file"
+    group = "Archive"
+    archiveName = "basic-demo-1.0.zip"
+
+    from("src")
 }
